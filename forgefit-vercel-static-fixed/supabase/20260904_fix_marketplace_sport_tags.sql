742eb43e95c42b84bb3ad2afec58f1e0ca81a623
gilbert21silva-hub <gilbert21silva@gmail.com>
2026-09-04 11:14:41 -0500

    Use table grant in sport tag migration

--- forgefit-vercel-static-fixed/supabase/20260904_fix_marketplace_sport_tags.sql
+++ forgefit-vercel-static-fixed/supabase/20260904_fix_marketplace_sport_tags.sql
@@ -2,5 +2,5 @@
 alter table public.marketplace_listings
   add column if not exists sport_tags text[] not null default '{}';
 
-grant select,insert,update (sport_tags) on public.marketplace_listings to authenticated;
+grant select,insert,update on public.marketplace_listings to authenticated;
 notify pgrst, 'reload schema';
